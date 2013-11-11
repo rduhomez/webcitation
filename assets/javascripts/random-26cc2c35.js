2393950d7654145adc7b8e7518a0c75af57f0aa8
David Leuliette <dleuliette@gmail.com>
2013-11-11 23:39:48 +0100

    Site updated to 33d7626

--- assets/javascripts/random-26cc2c35.js
+++
@@ -1,1 +0,0 @@
-!function(){$(document).ready(function(){return $.get("quotes.json",function(t){var e,n,o,r;return o=t,console.log(t),r=Math.floor(Math.random()*o.length),e=o[r],$("#js-author").html(e.author),$("#js-title").html(e.title),(""!==e.twitter||void 0!==e.twitter)&&(n='<a href="'+e.url+'">'+e.twitter+"</a>",$("#js-twitter").append(n)),$("#js-content").fadeIn()})})}.call(this);
